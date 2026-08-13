076787e460e75688bbc1b0f12c272300c36eda3e
Diya khandelwal <diyakhandelwal50@gmail.com>
2026-08-13 20:01:59 +0530

    Add state with useState

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import MovieCard from "./components/MovieCard";
 
 function App() {
   const movieName = "The Dark Knight";
   const rating = 9.0;
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       <h1>My Movie App</h1>
+
       <h2>{movieName}</h2>
       <p>Rating: {rating}/10</p>
+
       <button>Watch Now</button>
+
       <MovieCard title="Inception" rating={8.8} />
+
+      <p>Watch Count: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Watch
+      </button>
     </div>
   );
 }
