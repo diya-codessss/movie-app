f80ab250f9adc33cc2366ea4c382dca75bc1c5b1
Diya khandelwal <diyakhandelwal50@gmail.com>
2026-08-14 21:48:07 +0530

    Add reset functionality

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -22,6 +22,10 @@ function App() {
       <button onClick={() => setCount(count + 1)}>
         Watch
       </button>
+
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
     </div>
   );
 }
